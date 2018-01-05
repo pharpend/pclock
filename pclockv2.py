03954ac5fce82cdcd64f04c3b59af162976fa386
Peter Harpending <peter@harpending.org>
2018-01-04 17:39:38 -0700

    Delete trailing whitespace

--- pclockv2.py
+++ pclockv2.py
@@ -62,7 +62,7 @@ class Project:
         t = time.strftime('%c %z',time.localtime())
         self.entries[no_clockout_idx].out_time = t
         print(t ,'\n')
-        
+
     def clocked_out(self):
         """Boolean function
         Returns a truth value for whether clocked in or not"""
@@ -70,8 +70,8 @@ class Project:
         no_clockout_indeces = [i for i in range(0,ell) if None == self.entries[i].out_time]
 
         return( 0 == (len(no_clockout_indeces)) )
-   
-            
+
+
 class Journal:
     def __init__(self, projects = []):
         """Instanciation of Journal class
@@ -80,8 +80,8 @@ class Journal:
         projects -- list of current working entries denoted as a Journal (Default is set to empty)
         """
         self.projects = projects
-        
-       
+
+
     def from_yaml(yaml_string):
         """Read from yaml file and convert to Journal object"""
         J = Journal()
@@ -89,7 +89,7 @@ class Journal:
 
         for (k,v) in data_load.items():
             proj = Project(k)
-            
+
             for e in v:
                 #print(e)  #NST: Print all in an out times for all entries
                 ety = Entry(**e)
@@ -103,7 +103,7 @@ class Journal:
     def add_project(prj):
         """Create a new entry in the journal, a new project"""
         pass
-    
+
     def display_projects(jnl):
         """Print out a list of all working entries of a journal"""
 ##      for p in jnl.projects:
@@ -115,7 +115,7 @@ class Journal:
 
 def main():
     """Main method
-    
+
     Creates menus
     Opens and reads yaml file that contains entries and times
     Stores string from yaml file as a Journal object
@@ -125,59 +125,59 @@ def main():
     yaml_file = open(homedir + '/PrgProjects/pclock/sched1.yaml','r')
     yaml_file_contents = yaml_file.read()
     jnl = Journal.from_yaml(yaml_file_contents)
-    
+
     print('''\n pclock, v0.0.0
     i = clock in
     o = clock out
     p = projects
     s = project status
     q, x, exit, quit = exit pclock \n''')
-   
+
     while True:
         ipt = input('pclock> ')
         ipt = ipt.strip()
-        
+
         if 'i' == ipt:
             print('\nSelect a project to clock into: ')
             prjs = jnl.projects
             num_projects = len(prjs)
-            
+
             for i in range(num_projects):
                 this_prj = prjs[i]
                 print(i, this_prj.name)
-                
+
             n = input('\nchoose a project: ')
             num = int(n)
-            
+
             if  num >= len(prjs):
                 print('\nnot in list \n')
             else:
                 print('\n',prjs[num].name)
                 prjs[num].clock_in()
-                
-            
+
+
         elif 'o' == ipt:
             print('\nWhich project would you like to clock out of? ')
             prjs = jnl.projects
             num_projects = len(prjs)
-            
+
             for i in range(num_projects):
                 this_prj = prjs[i]
                 print(i, this_prj.name)
-                
+
             n = input('\nchoose a project to clock out: ')
             num = int(n)
-            
+
             if  num >= len(prjs):
                 print('\nnot in list \n')
             else:
                 print('\n',prjs[num].name)
                 prjs[num].clock_out()
-            
+
         elif 'p' == ipt:
             print('\nYour current projects: ')
             Journal.display_projects(jnl)
-            
+
         elif 's' == ipt:
             print('\nStatus of projects:\n')
             for p in jnl.projects:
@@ -187,10 +187,10 @@ def main():
                     print(p.name,'is clocked out.\n')
                 else:
                     print(p.name,'has not been clocked out.\n')
-            
+
         elif ipt in ['q','x','exit','quit']:
             print('Logged out of session ')
             break
-        
+
 if '__main__' == __name__ :
     main()
